944b410274332021c920b3af4ef6c1106e55ddd5
Muneeb244 <91079744+Muneeb244@users.noreply.github.com>
2022-06-11 19:52:01 +0500

    gg

--- Test.java
+++ Test.java
@@ -1,5 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello");
+        JFrame frame = new JFrame("Vehicle Showroom Management System");
+        JPanel panel = new JPanel(null);
+
+        JLabel main_label = new JLabel("Welcome to");
+        JLabel main_label2 = new JLabel("Vehicle Showroom Management System");
+        main_label.setFont(new Font("Aerial", Font.BOLD, 30));
+        main_label2.setFont(new Font("Serif", Font.BOLD, 40));
+        main_label.setForeground(Color.CYAN);
+        main_label2.setForeground(Color.CYAN);
+
+        main_label.setBounds(400, 60, 500, 100);
+        main_label2.setBounds(150, 100, 700, 100);
+
+        JButton starting_button = new JButton("Get Started");
+        starting_button.setBounds(300, 500, 400, 100);
+        starting_button.setBackground(Color.CYAN);
+        starting_button.setFont(new Font("Aerial", Font.BOLD, 20));
+
+//        main_label.setForeground(Color.white);
+        panel.add(starting_button);
+        panel.add(main_label);
+        panel.add(main_label2);
+        panel.setBackground(new Color(21, 40, 51));
+
+
+        frame.add(panel);
+        frame.setVisible(true);
+        frame.setSize(1000, 800);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
     }
 }
