b0e93a2a874c9fdeb2ec1488c5dea7e7c5752824
mUne3b1 <amuneeb818@gmail.com>
2022-06-18 22:39:59 +0500

    gg

---
+++ Admin_Manage_Exchange_Board.java
@@ -0,0 +1,89 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Admin_Manage_Exchange_Board {
+    private JFrame frame;
+    private JPanel panel;
+    private JLabel main_label;
+    private JButton addcar_button;
+
+    private JButton viewcar_button;
+    private JButton deletecar_button;
+    private JButton back_button;
+    public Admin_Manage_Exchange_Board(){
+        frame = new JFrame("Vehicle Showroom Management System");
+        panel = new JPanel(null);
+
+        main_label = new JLabel("Manage Exchange Vehicles");
+        main_label.setFont(new Font("Serif", Font.BOLD, 35));
+        main_label.setForeground(Color.CYAN);
+        main_label.setBounds(430, 30, 700, 100);
+
+
+        addcar_button = new JButton("Add cars");
+        addcar_button.setBounds(300, 130, 450, 90);
+        addcar_button.setBackground(Color.CYAN);
+        addcar_button.setFont(new Font("Aerial", Font.BOLD, 25));
+        addcar_button.addActionListener(new Admin_Manage_Exchange_Board.Handler());
+
+        viewcar_button = new JButton("View Cars");
+        viewcar_button.setBounds(300, 420, 450, 90);
+        viewcar_button.setBackground(Color.CYAN);
+        viewcar_button.setFont(new Font("Aerial", Font.BOLD, 25));
+        viewcar_button.addActionListener(new Admin_Manage_Exchange_Board.Handler());
+
+        deletecar_button = new JButton("Delete cars");
+        deletecar_button.setBounds(300, 270, 450, 90);
+        deletecar_button.setBackground(Color.CYAN);
+        deletecar_button.setFont(new Font("Aerial", Font.BOLD, 25));
+        deletecar_button.addActionListener(new Admin_Manage_Exchange_Board.Handler());
+
+        back_button = new JButton("Back");
+        back_button.setBounds(435, 690, 180, 60);
+        back_button.setBackground(Color.CYAN);
+        back_button.setFont(new Font("Aerial", Font.BOLD, 20));
+        back_button.addActionListener(new Admin_Manage_Exchange_Board.Handler());
+
+        panel.add(main_label);
+        panel.add(addcar_button);
+        panel.add(viewcar_button);
+        panel.add(deletecar_button);
+        panel.add(back_button);
+        panel.setBackground(new Color(21, 40, 51));
+        frame.add(panel);
+        frame.setVisible(true);
+        frame.setSize(1000, 800);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+    }
+    class Handler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getSource() == addcar_button){
+                frame.dispose();
+                Admin_Add_Car_GUI admin_add_car_gui = new Admin_Add_Car_GUI();
+            }
+            if(e.getSource() == updatecar_button){
+                frame.dispose();
+                updateCarsGUI updateCarsGUI = new updateCarsGUI();
+            }
+            if (e.getSource() == viewcar_button) {
+                frame.dispose();
+                Admin_View_Car_GUI admin_view_car_gui = new Admin_View_Car_GUI();
+            }
+            if(e.getSource() == deletecar_button){
+                frame.dispose();
+                Admin_Delete_Car_GUI admin_delete_car_gui = new Admin_Delete_Car_GUI();
+            }
+            if(e.getSource() == back_button){
+                frame.dispose();
+                Admin_Manage_Vehicles admin_manage_vehicles = new Admin_Manage_Vehicles();
+            }
+        }
+    }
+}
+}
--- Tester_My.java
+++ Tester_My.java
@@ -1,8 +1,9 @@
 public class Tester_My {
     public static void main(String[] args){
 //        Admin_Manage_Vehicles m = new Admin_Manage_Vehicles();
-        updateCarsGUI ne = new updateCarsGUI();
+      //  updateCarsGUI ne = new updateCarsGUI();
         //Admin_Manage_Customer_Board n = new Admin_Manage_Customer_Board();
+        Admin_View_Sales_Report n = new Admin_View_Sales_Report();
     }
 
 }
