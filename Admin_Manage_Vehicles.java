1d4108748ac630453841e9fa22c500c61eb70fcd
mUne3b1 <amuneeb818@gmail.com>
2022-06-19 18:47:33 +0500

    gg

--- Admin_Manage_Vehicles.java
+++ Admin_Manage_Vehicles.java
@@ -9,6 +9,7 @@ public class Admin_Manage_Vehicles {
     private JButton manage_motorbikes;
     private JButton back_button;
     private JButton manage_used_vehicles;
+    private JButton  manage_test_drive;
     private JLabel main_label;
 
     public Admin_Manage_Vehicles() {
@@ -21,27 +22,35 @@ public class Admin_Manage_Vehicles {
 
         //Manage cars
         manage_cars = new JButton("Manage Cars");
-        manage_cars.setBounds(300, 150, 450, 90);
+        manage_cars.setBounds(300, 150, 400, 70);
         manage_cars.setBackground(Color.CYAN);
         manage_cars.setFont(new Font("Aerial", Font.BOLD, 30));
         manage_cars.addActionListener(new Handler());
 
         //Manage_Motorbikes
         manage_motorbikes = new JButton("Manage Motorbikes");
-        manage_motorbikes.setBounds(300, 300, 450, 90);
+        manage_motorbikes.setBounds(300, 250, 400, 70);
         manage_motorbikes.setBackground(Color.CYAN);
         manage_motorbikes.setFont(new Font("Aerial", Font.BOLD, 30));
         manage_motorbikes.addActionListener(new Handler());
 
         // Manage used
         manage_used_vehicles = new JButton("Manage Used Vehicles");
-        manage_used_vehicles.setBounds(300, 450, 450, 90);
+        manage_used_vehicles.setBounds(300, 350, 400, 70);
         manage_used_vehicles.setBackground(Color.CYAN);
         manage_used_vehicles.setFont(new Font("Aerial", Font.BOLD, 30));
         manage_used_vehicles.addActionListener(new Handler());
 
+        // Manage Test Drive
+
+        manage_test_drive = new JButton("Manage Test Drive");
+        manage_test_drive.setBounds(300, 450, 400, 70);
+        manage_test_drive.setBackground(Color.CYAN);
+        manage_test_drive.setFont(new Font("Aerial", Font.BOLD, 30));
+        manage_test_drive.addActionListener(new Handler());
+
         back_button = new JButton("Back");
-        back_button.setBounds(450, 590, 180, 60);
+        back_button.setBounds(400, 560, 180, 60);
         back_button.setBackground(Color.CYAN);
         back_button.setFont(new Font("Aerial", Font.BOLD, 20));
         back_button.addActionListener(new Handler());
@@ -50,6 +59,7 @@ public class Admin_Manage_Vehicles {
         panel.add(manage_cars);
         panel.add(manage_used_vehicles);
         panel.add(manage_motorbikes);
+        panel.add(manage_test_drive);
         panel.add(back_button);
         panel.add(main_label);
         frame.add(panel);
@@ -74,6 +84,10 @@ public class Admin_Manage_Vehicles {
             if(e.getSource() == manage_used_vehicles){
                 frame.dispose();
                 Admin_Manage_Used_Vehicle_Board admin_manage_used_vehicle_board = new Admin_Manage_Used_Vehicle_Board();
+            }
+            if(e.getSource() == manage_test_drive){
+                frame.dispose();
+              Admin_Manage_Test_Drive admin_manage_test_drive = new Admin_Manage_Test_Drive();
             }
             if(e.getSource() == back_button){
                 frame.dispose();
